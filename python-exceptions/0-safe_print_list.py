911440519b5ccab2344544d328a6da35a75f074e
mehdiicyber <13735@holbertonstudents.com>
2026-04-18 10:23:33 +0400

    Update 0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    count = 0
+    printed_count = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
-            count += 1
+            printed_count += 1
         except IndexError:
             break
-    print("")  # Sonda yeni sətir çap edir
-    return count
+    
+    print("") 
+    return (printed_count)
