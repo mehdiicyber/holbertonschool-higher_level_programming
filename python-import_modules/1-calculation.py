ca52ddc8072bd162f1c9449c972ff5f4f28c9a11
mehdiicyber <13735@holbertonstudents.com>
2026-04-21 21:04:46 +0400

    Update 1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,24 +1,11 @@
 #!/usr/bin/python3
-from add_0 import add
-from add_0 import sub
-from add_0 import mul
-from add_0 import div
+from calculator_1 import add, sub, mul, div
+
 if __name__ == "__main__":
     a = 10
     b = 5
+
     print("{} + {} = {}".format(a, b, add(a, b)))
-
-if __name__ == "__main__":
-    a = 10
-    b = 5
     print("{} - {} = {}".format(a, b, sub(a, b)))
-
-if __name__ == "__main__":
-    a = 10
-    b = 5
     print("{} * {} = {}".format(a, b, mul(a, b)))
-
-if __name__ == "__main__":
-    a = 10
-    b = 5
     print("{} / {} = {}".format(a, b, div(a, b)))
