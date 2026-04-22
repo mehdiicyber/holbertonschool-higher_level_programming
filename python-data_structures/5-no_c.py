4b7e773b2319d1502e7d6092953f2068c025525b
mehdiicyber <13735@holbertonstudents.com>
2026-04-22 12:37:19 +0000

    Update

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    x = ""
+    for i in my_string:
+        if i == "c" or i == "C"
+            continue
+        else:
+            x = x + i
+    return x
