c71acb3019be66b4c4d8dd22779d72ce64ac3850
mehdiicyber <13735@holbertonstudents.com>
2026-04-22 16:52:27 +0400

    Update 6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,11 +3,8 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in range(len(row)):
-            # Print the integer. Use end=" " to keep them on the same line.
-            # We use a condition to avoid a trailing space at the end of the row.
             if i != len(row) - 1:
                 print("{:d}".format(row[i]), end=" ")
             else:
                 print("{:d}".format(row[i]), end="")
-        # After the inner loop finishes a row, print a newline
         print()
