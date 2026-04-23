0b982fd62b6f50178763d5a6916befd6fa8a9b5d
mehdiicyber <13735@holbertonstudents.com>
2026-04-23 21:17:50 +0000

    Update

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    print("""Number: {}\n""
+        ""ids: {}\n""
+        ""language: {}\n""
+        ""track: {}\n""".format(a_dictionary['Number'], a_dictionary['ids'], a_dictionary['language'], a_dictionary['track'])
