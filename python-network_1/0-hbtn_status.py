1225cde4fd0d990ed90079bceae9daa559e83d03
mehdiicyber <13735@holbertonstudents.com>
2026-07-01 23:56:04 +0400

    Update 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 """
-A script that fetches https://intranet.hbtn.io/status using urllib.
+Fetches https://intranet.hbtn.io/status
 """
 import urllib.request
 
 
 if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
-    
+    url = 'https://intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
-        content = response.read()
-        
+        html = response.read()
+
     print("Body response:")
-    print(f"\t- type: {type(content)}")
-    print(f"\t- content: {content}")
-    print(f"\t- utf8 content: {content.decode('utf-8')}")
+    print(f"\t- type: {type(html)}")
+    print(f"\t- content: {html}")
+    print(f"\t- utf8 content: {html.decode('utf-8')}")
