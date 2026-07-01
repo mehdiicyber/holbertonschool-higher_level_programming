2cf08b14bde2b3b1f233fec21aaff9b241defa0e
mehdiicyber <13735@holbertonstudents.com>
2026-07-02 00:01:25 +0400

    Update 1-hbtn_header.py

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -9,6 +9,6 @@ import urllib.request
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    
+
     with urllib.request.urlopen(url) as response:
         print(response.headers.get('X-Request-Id'))
